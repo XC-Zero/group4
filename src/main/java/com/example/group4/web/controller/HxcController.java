2d7e92b88289c386375b3145d06377517037e9c7
HUAXC <571927932@qq.com>
2019-12-24 16:40:47 +0800

    hhhhh

--- src/main/java/com/example/group4/web/controller/HxcController.java
+++ src/main/java/com/example/group4/web/controller/HxcController.java
@@ -1,7 +1,7 @@
 package com.example.group4.web.controller;
 
 public class HxcController {
-    public void main(String[] args){
+    public static void main(String[] args){
         System.out.println("hxc建的类");
 
     }
