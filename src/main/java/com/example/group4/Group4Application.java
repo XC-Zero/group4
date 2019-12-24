ed929deae5b04ea96acecf831ac14a5790d4a667
hzx <1285180855@qq.com>
2019-12-24 17:35:23 +0800

    haha

--- src/main/java/com/example/group4/Group4Application.java
+++ src/main/java/com/example/group4/Group4Application.java
@@ -1,4 +1,4 @@
-package com.example.group4.web;
+package com.example.group4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
