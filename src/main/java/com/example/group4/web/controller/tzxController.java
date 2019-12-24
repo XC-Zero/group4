c589815944ebb6b13d50225184717725a4fcf45a
taozx <tzx123567@163.com>
2019-12-24 16:15:12 +0800

    绣花该

--- src/main/java/com/example/group4/web/controller/tzxController.java
+++ src/main/java/com/example/group4/web/controller/tzxController.java
@@ -2,7 +2,6 @@ package com.example.group4.web.controller;
 
 public class tzxController {
     //世界上最帅的男人
-
     public static void main(String[] args) {
         System.out.println("陶中翔最帅");
     }
