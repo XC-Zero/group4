b824f52b33b2d7286df0600e1dbeb6d435693012
XC-Zero <645171033@qq.com>
2019-12-25 08:59:41 +0800

    xc

--- src/main/java/com/example/group4/Controller/LoginController.java
+++ src/main/java/com/example/group4/Controller/LoginController.java
@@ -1,10 +1,13 @@
 package com.example.group4.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LoginController {
-    public static boolean Login(){
-        return false;
+    @RequestMapping("Login")
+    public static String Login(){
+        return "ERROR";
     }
 }
