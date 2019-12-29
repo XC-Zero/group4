b06c4e43187d6d2efc9c268853965c452eb89843
HUAXC <571927932@qq.com>
2019-12-29 14:54:40 +0800

    hxc

--- src/main/java/com/example/group4/service/Impl/TeacherServiceImpl.java
+++ src/main/java/com/example/group4/service/Impl/TeacherServiceImpl.java
@@ -29,12 +29,32 @@ public class TeacherServiceImpl implements ITeacherService {
     @Override
     public List<Teacher> search(String key, String word) throws RuntimeException {
         word=word==null?"":word;    //模糊查询
-
+        TeacherExample teacherExample=new TeacherExample();
         //select * from cms_article where titile like %% 就是查询所有
-        if ((key==null||"".equals(key))&&(word==null||"".equals(word))) {
-            TeacherExample teacherExample = new TeacherExample();
+        if ((key==null||"".equals(key))&&(word==null||"".equals(word))){
+
             return teacherMapper.selectByExample(teacherExample);
 
+        }
+        else if ((key==null||"".equals(key)&&!"".equals(word))){//前面下拉框为空，后面输入框不为空
+            //根据作者或标题查询
+            word="%"+word+"%";
+            teacherExample.createCriteria().andNameLike(word);
+
+
+            return teacherMapper.selectByExample(teacherExample);
+
+
+        }else if ("id".equals(key)){
+            word="%"+word+"%";
+            int id=Integer.parseInt(word);
+            teacherExample.createCriteria().andIdEqualTo(id);
+            return teacherMapper.selectByExample(teacherExample);
+
+        }else if ("name".equals(key)){
+            word="%"+word+"%";
+            teacherExample.createCriteria().andNameLike(word);
+            return teacherMapper.selectByExample(teacherExample);
         }
         return null;
 
@@ -44,11 +64,22 @@ public class TeacherServiceImpl implements ITeacherService {
 
     @Override
     public void saveOrUpdate(Teacher teacher) throws RuntimeException {
+        if (teacher==null){
+            throw new RuntimeException("参数为空");
+        }
+        if (teacher.getId()==null){
+            teacherMapper.insert(teacher);
+
+        }else {
+            teacherMapper.updateByPrimaryKey(teacher);
+
+        }
 
     }
 
     @Override
     public void deleteById(int id) throws RuntimeException {
+       teacherMapper.deleteByPrimaryKey(id);
 
     }
 }
--- src/main/java/com/example/group4/web/controller/TeacherController.java
+++ src/main/java/com/example/group4/web/controller/TeacherController.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,11 +28,29 @@ public Message findAll(){
 
     }
 
-    @GetMapping("/selectByAll")
+    @GetMapping("/selectByAId")
     @ApiOperation("查询id")
     public Message findById(int id){
 
         return MessageUtil.success(teacherService.findById(id));
 
     }
+    @GetMapping("/deleteById")
+    @ApiOperation("删除id")
+    public Message deleteById(int id){
+        teacherService.deleteById(id);
+        return MessageUtil.success();
+
+    }
+    @PostMapping("/updateOrAdd")
+    @ApiOperation("更新或添加")
+    public Message updateOrAdd(Teacher teacher){
+        teacherService.saveOrUpdate(teacher);
+        return MessageUtil.success();
+
+
+
+    }
+
+
 }
