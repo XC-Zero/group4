ac2e731ffdc38b589098f315a6cf4437046e281b
HUAXC <571927932@qq.com>
2019-12-29 11:32:56 +0800

    hxc

--- src/main/java/com/example/group4/config/Swagger2Config.java
+++ src/main/java/com/example/group4/config/Swagger2Config.java
@@ -23,7 +23,7 @@ public class Swagger2Config {
     public Docket demoApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.group4.Controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.example.group4.web.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
