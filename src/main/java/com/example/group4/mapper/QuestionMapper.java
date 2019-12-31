1dd102a45b3da38daf6566169178cfe33226351b
XC-Zero <645171033@qq.com>
2019-12-31 09:29:31 +0800

    xc2

--- src/main/java/com/example/group4/mapper/QuestionMapper.java
+++ src/main/java/com/example/group4/mapper/QuestionMapper.java
@@ -2,9 +2,8 @@ package com.example.group4.mapper;
 
 import com.example.group4.bean.Question;
 import com.example.group4.bean.QuestionExample;
-import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface QuestionMapper {
     long countByExample(QuestionExample example);
@@ -28,5 +27,4 @@ public interface QuestionMapper {
     int updateByPrimaryKeySelective(Question record);
 
     int updateByPrimaryKey(Question record);
-
 }
