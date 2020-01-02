1c451e62b6a359decbc9c810a95df3af01c29673
HUAXC <571927932@qq.com>
2020-01-02 08:53:52 +0800

    hxc

--- src/main/java/com/example/group4/mapper/ex/QueryCheckEXMapper.java
+++ src/main/java/com/example/group4/mapper/ex/QueryCheckEXMapper.java
@@ -1,7 +1,5 @@
 package com.example.group4.mapper.ex;
 
-import com.example.group4.bean.ex.SurveyCheckEX;
-
 import java.util.List;
 
 public interface QueryCheckEXMapper {
--- src/main/java/com/example/group4/service/ISurveyCheckService.java
+++ src/main/java/com/example/group4/service/ISurveyCheckService.java
@@ -1,7 +1,5 @@
 package com.example.group4.service;
 
-import com.example.group4.bean.ex.SurveyCheckEX;
-
 import java.util.List;
 
 public interface ISurveyCheckService {
